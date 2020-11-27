b3c758efe88d1f2f54b580d632ed61775f54e0f6
denver.html <denver.html@daum.net>
2020-11-27 15:41:14 +0900

    주석 처리되었습니다.

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -12,26 +12,22 @@ const Search = (props) => {
   const { attribute } = props
   // console.log('components → [Search.js] → attribute: ', attribute)
   // console.log('components → [Search.js] → !!attribute.select: ', !!attribute.select)
-  // console.log('')
 
   const [select, setSelect] = useState(!!attribute.select ? attribute.select : 'subject')
   const [keyword, setKeyword] = useState(!!attribute.keyword ? attribute.keyword : '')
 
   const onChange = (event) => {
     // console.log('components → [Search.js] → event.target.tagName: ', event.target.tagName)
-    // console.log('')
 
     if (event.target.tagName.toLowerCase() === 'select') {
       // console.log('event.target.options[event.target.selectedIndex].text: ', event.target.options[event.target.selectedIndex].text)
       // console.log('event.target.options[event.target.selectedIndex].value: ', event.target.options[event.target.selectedIndex].value)
-      // console.log('')
 
       setSelect(event.target.options[event.target.selectedIndex].value)
     }
 
     if (event.target.tagName.toLowerCase() === 'input') {
       // console.log('event.target.value: ', event.target.value)
-      // console.log('')
 
       setKeyword(event.target.value)
     }
